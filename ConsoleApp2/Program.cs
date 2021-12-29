3938ccbe9c519b6aa57fe09255cc0fb5c6f323fa
Pavkov <pawko774@gmail.com>
2021-12-29 15:35:44 +0100

    wiadomość

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("W tym kalkulatorze można używać tylko liczb całkowitych i działań \'+\' \'-\' \'*\' \'/\'");
             kalkutor siema = new kalkutor();
 
         }
