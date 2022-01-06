6507c0e680a28e647e4d0d572acb18530491a6a4
Pavkov <pawko774@gmail.com>
2022-01-06 01:24:02 +0100

    coraz bliże końca

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -15,7 +15,18 @@ namespace ConsoleApp2
            
         }
 
-        
+        void wypełnianie()
+        {
+            for(int i = 0;i<liczby.Length; i++)
+            {
+                liczby[i] = "0";
+            }
+
+            for (int i = 0; i < znaki.Length; i++)
+            {
+                znaki[i] = "0";
+            }
+        }
 
         public void literacja()
         {
@@ -43,19 +54,21 @@ namespace ConsoleApp2
             liczby[licznik] = liczba;
             //Console.WriteLine(ll);
 
-            foreach (string i in liczby)
+            /*foreach (string i in liczby)
             {
                 Console.WriteLine(i);
             }
             foreach (string i in znaki)
             {
                 Console.WriteLine(i);
-            }
+            }*/
 
         }
         public void liczenie()
         {
+            int lenght = wyrażenie.Length;
             int lenght2 = znaki.Length;
+
             for(int i = 0;i<lenght2; i++)
             {
                 int where;
@@ -63,7 +76,10 @@ namespace ConsoleApp2
                 int b;
                 int c;
                 string cs;
-                
+                string przesuwanie;
+                string usuwanie = "0";
+
+
                  where = Array.IndexOf(znaki, "*");
                 
                 //Console.WriteLine(where);
@@ -106,18 +122,39 @@ namespace ConsoleApp2
                 //Console.WriteLine(where);
                 if (where >= 0)
                 {
+                    int d = where;
+                    int e;
                     znaki[where] = "0";
                     a = Convert.ToInt32(liczby[where]);
                     liczby[where] = "0";
                     where += 1;
                     b = Convert.ToInt32(liczby[where]);
                     liczby[where] = "0";
+                    e = where+1;
                     c = a + b;
-                    for(int x = 0;x<=lenght2-(where+1);x++)
+                    for(int x = 0;x<lenght2-1;x++)
                     {
-                        //Console.WriteLine(znaki[x]);
+                        d += 1;
+                        przesuwanie = znaki[d];
+                        znaki[d] = usuwanie;
+                        where -= 1;
+                        znaki[d] = przesuwanie;
+                        where += 2;
+
+                        Console.WriteLine(znaki[x]);
                     }
-                    //Console.WriteLine(c);
+                    for(int x = 0;x<lenght-3; x++)
+                    {
+                        
+                        przesuwanie = liczby[e];
+                        liczby[e] = usuwanie;
+                        e -= 1;
+                        znaki[e] = przesuwanie;
+                        e += 2;
+
+                        Console.WriteLine(liczby[x]);
+                    }
+                    Console.WriteLine(c);
                     where -= 1;
                     cs = c.ToString();
                     liczby[where] = cs;
